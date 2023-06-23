81bf68ab0808f7c896b1427db63cab05f036036a
quinterodiego <d86webs@gmail.com>
2023-06-22 23:06:24 -0300

    asd

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css';
-import Contenido from './Contenido';
-import Encabezado from './Encabezado';
-import Producto from './Producto';
+import './App.css'
 
-function App() {
+export const App = () => {
   return (
-    <div className="container my-5">
-      <div className="row text-center mb-5">
-        <h1><Encabezado /></h1>
+    <div className="container">
+      <h1>Mio Mio</h1>
+      <button className="btn btn-outline-secondary">Carrito</button>
+      <hr />
+      <input type="text" class="form-control" placeholder="Tu búsqueda..." aria-label="Username" aria-describedby="basic-addon1"></input>
+      <div className="img">
+        <img src="./img/Anteojos/ali-pazani-GwglcplmXDs-unsplash.jpg" alt="" />
+        <div className="title"><p>Lorem, ipsum dolor.</p></div>
       </div>
-      <div className="row">
-        <div className="col-md-8">
-          <Contenido producto={"Coca Cola"} tipo={"Light"} precio={500} />
-        </div>
-        <div className="col-md-4">
-          <Producto />
-        </div>
+      <div className="img">
+        <img src="./img/Anteojos/ali-pazani-GwglcplmXDs-unsplash.jpg" alt="" />
+        <div className="title"><p>Lorem, ipsum dolor.</p></div>
+      </div>
+      <div className="img">
+        <img src="./img/Anteojos/ali-pazani-GwglcplmXDs-unsplash.jpg" alt="" />
+        <div className="title"><p>Lorem, ipsum dolor.</p></div>
       </div>
     </div>
-  );
+  )
 }
-
-export default App;
