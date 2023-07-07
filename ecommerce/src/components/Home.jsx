b4d92d7ef35717d964804032e23e918dbb9a27c8
quinterodiego <d86webs@gmail.com>
2023-07-06 23:09:21 -0300

    asd

--- ecommerce/src/components/Home.jsx
+++ ecommerce/src/components/Home.jsx
@@ -8,13 +8,12 @@ export const Home = () => {
             <div className="text-center">
                 <h2>PRODUCTOS</h2>
                 
-                <div className="d-flex flex-row flex-wrap w-100">
                 {
                     products.map((product) => (
                         <Link to={'/product/' + product.id}>
-                            <div className="card w-25" key={product.id}>
-                                <img className="card-img-top" src={product.thumbnail} alt="Card image cap" style={{"width": "300px"}}/>
-                                <div className="card-body">
+                            <div className="card d-flex flex-row" key={product.id}>
+                                <img className="card-img-top" src={product.thumbnail} alt="Card image cap" style={{"width": "30%"}}/>
+                                <div className="card-body w-50">
                                     <h5 className="card-title">{product.title}</h5>
                                     <p className="card-text">{product.description}</p>
                                     <p className="card-text"><b>${product.price}</b></p>
@@ -24,7 +23,6 @@ export const Home = () => {
                         </Link>
                     ))
                 }
-                </div>
             </div>
         </div>
     )
